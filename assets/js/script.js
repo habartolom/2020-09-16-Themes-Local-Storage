2dc5c873bc771d4489afbafa47e943afb72a0ad4
Harold Andres Bartolo Moscoso <habartolom@gmail.com>
2020-09-17 09:10:44 -0500

    Optimización código

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@ const dark = document.getElementById('dark');
 const light = document.getElementById('light');
 const oceanic = document.getElementById('oceanic');
 const blue = document.getElementById('blue');
+const container = document.getElementById('container');
 
 /**
  * Obtiene el último tema establecido y lo aplica al cargar la página
  */
 
-let theme = localStorage.getItem('theme');
-document.getElementById('container').classList.add(theme);
-
+let currentTheme = localStorage.getItem('theme');
+if(currentTheme != null){
+    applytheme(currentTheme);
+}
 
 /* Eventos click para cada uno de los temas */
 
@@ -32,19 +34,15 @@ blue.addEventListener('click', ()=>{
 
 /**
  * Si el tema es light quita el tema guardado en localStorage y su clase respectiva en el container, 
- * Si el tema es distinto a light, guarda el nuevo tema en localStorage, se remueve la clase del tema anterior si la hay y se añade la nueva clase al container
+ * Si el tema es distinto a light, guarda el nuevo tema en localStorage, y se establece la nueva clase al container
  */
 function applytheme(selectedTheme){
     if(selectedTheme != 'light'){
-        let currentTheme = localStorage.getItem('theme');
-        document.getElementById('container').classList.remove(currentTheme);
-        
         localStorage.setItem('theme', selectedTheme);
-        document.getElementById('container').classList.add(selectedTheme)
+        container.className = 'container ' + selectedTheme;
     }
     else{
-        let currentTheme = localStorage.getItem('theme');
-        document.getElementById('container').classList.remove(currentTheme);
         localStorage.removeItem('theme');
+        container.className = 'container';
     }
 }
